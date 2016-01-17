c18e350086915343712fa2129463761763a0b83a
Michael Dewberry <michael.dewberry@gmail>
2016-01-16 21:24:11 -0500

    Fix overaggressive dwelling

--- app/Source/LaserPointOptimizer.cpp
+++ app/Source/LaserPointOptimizer.cpp
@@ -85,6 +85,10 @@ void LaserPointOptimizer::fillBufferFromFrame(LaserOutputBuffer& buffer)
             if (item.origin.y > 1.0)  { item.origin.y = 1.0; }
 
             if (item.type == PatternType::Polyline) {
+                if (item.points.size() == 0)
+                {
+                    continue;
+                }
 
                 int16_t ptX = item.points[0].x * scale;
                 int16_t ptY = item.points[0].y * scale;
@@ -105,7 +109,7 @@ void LaserPointOptimizer::fillBufferFromFrame(LaserOutputBuffer& buffer)
 
                     int distance = sqrt(pow(prevItemX - ptX, 2) + pow(prevItemY - ptY, 2));
                     int numSegments = std::max(1, distance / longestUnbrokenLine);
-                    addLine(buffer.points, dwellOffPoints, dwellOnPoints, numSegments, prevItemX, prevItemY, ptX, ptY, item.red, item.green, item.blue, intensity);
+                    addLine(buffer.points, 0, internalShapeDwellPoints, numSegments, prevItemX, prevItemY, ptX, ptY, item.red, item.green, item.blue, intensity);
 
                     prevItemX = ptX;
                     prevItemY = ptY;
